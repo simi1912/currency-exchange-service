0919a339c335292bf531de1bebf31c0ac39a826a
Simi <nioasca@gmail.com>
2021-04-12 18:34:50 +0300

    Added port value to be returned by endpoint.

--- src/main/java/com/simi/microservices2/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/simi/microservices2/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,6 @@
 package com.simi.microservices2.currencyexchangeservice;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,18 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Environment environment;
+
+    public CurrencyExchangeController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue( @PathVariable("from") String from,
                                                 @PathVariable("to") String to){
-        return new ExchangeValue(100L, from, to, BigDecimal.valueOf(65));
+        ExchangeValue exchangeValue = new ExchangeValue(100L, from, to, BigDecimal.valueOf(65));
+        exchangeValue.setPort(Integer.valueOf(environment.getProperty("local.server.port")));
+        return exchangeValue;
     }
 
 }
