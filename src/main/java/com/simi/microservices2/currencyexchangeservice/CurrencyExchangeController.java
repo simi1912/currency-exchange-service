24d73fbed601dd2c62168fefe847c6dc9aa09be0
simi1912 <nioasca@gmail.com>
2021-04-15 20:08:58 +0300

    Added Spring Sleuth.

--- src/main/java/com/simi/microservices2/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/simi/microservices2/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.simi.microservices2.currencyexchangeservice;
 
 import com.simi.microservices2.currencyexchangeservice.model.ExchangeValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ public class CurrencyExchangeController {
 
     private Environment environment;
     private ExchangeValueRepository repository;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public CurrencyExchangeController(Environment environment, ExchangeValueRepository repository) {
         this.environment = environment;
@@ -25,6 +28,8 @@ public class CurrencyExchangeController {
 
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.valueOf(environment.getProperty("local.server.port")));
+
+        logger.info("", exchangeValue);
 
         return exchangeValue;
     }
--- src/main/java/com/simi/microservices2/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/simi/microservices2/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -1,9 +1,11 @@
 package com.simi.microservices2.currencyexchangeservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,4 +15,8 @@ public class CurrencyExchangeServiceApplication {
         SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
     }
 
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
