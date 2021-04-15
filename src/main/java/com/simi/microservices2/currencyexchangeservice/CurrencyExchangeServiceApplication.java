937669b5d10cc1d7c95fb12eec0cbf26f2a6e224
simi1912 <nioasca@gmail.com>
2021-04-15 11:30:30 +0300

    Configured Eureka client.

--- src/main/java/com/simi/microservices2/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/simi/microservices2/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,8 +3,10 @@ package com.simi.microservices2.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
     public static void main(String[] args) {
